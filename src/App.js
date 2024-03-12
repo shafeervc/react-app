29aeb94c7aa0cb1c7f91d85fffe52f9225da4da5
Shabeer vaniakodiyil Chekkunni <shachekk@publicisgroupe.net>
2024-03-12 16:48:35 +0530

    added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React for Ampliy auto deployment !!!!!
+          Learn React for Ampliy auto deployment & redeploy automatically  !!!!!
         </a>
       </header>
     </div>
